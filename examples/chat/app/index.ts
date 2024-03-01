75c7aa0232bea3787db1f5dbc6cf098c036abecc
davemaier <david.maier@atempo.at>
2024-03-02 00:49:51 +0100

    fix errors from renaming

--- examples/chat/app/index.ts
+++ examples/chat/app/index.ts
@@ -1,7 +1,6 @@
 import { Motile } from "@motile";
 import { messages } from "./messages";
 import { users } from "./users";
-import "@kitajs/html/register";
 
 export const handlers = {
   ...users,
---
+++ examples/chat/app/users.ts
@@ -0,0 +1,34 @@
+import { Motile } from "@motile";
+import * as schema from "@motile/schema";
+
+export const users: Motile = {
+  users: {
+    insert: [
+      {
+        handler: async (record, db) => {
+          await db.insert(schema.messages).values({
+            messageText: `Hi I'm new here! My name is: ${record.username}`,
+            userId: record.userId,
+          });
+        },
+        name: "introduction",
+      },
+    ],
+    update: [
+      {
+        handler: async (record) => {
+          await fetch(
+            "https://hooks.slack.com/services/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",
+            {
+              method: "POST",
+              body: JSON.stringify({
+                text: `User ${record.email} was just updated`,
+              }),
+            }
+          );
+        },
+        name: "slack",
+      },
+    ],
+  },
+};
